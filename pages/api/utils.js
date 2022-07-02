ff41fad79f603c2d554a3ad846d61f8ee2965ea6
rks101 <rajendra.kr.solanki@gmail.com>
2022-07-02 21:07:48 +0530

    initial setup before uploading videos

---
+++ pages/api/utils.js
@@ -0,0 +1,28 @@
+import prisma from 'lib/prisma'
+import { faker } from '@faker-js/faker'
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') return res.end()
+
+    if (req.body.task === 'generate_content') {
+        let usersCount = 0
+
+        while (usersCount < 10) {
+            await prisma.user.create({
+                data: {
+                    name: faker.name.findName(),
+                    username: faker.internet.userName().toLowerCase(),
+                    email: faker.internet.email().toLowerCase(),
+                    image: faker.image.avatar(),
+                },
+            })
+            usersCount++
+        }
+    }
+
+    if (req.body.task === 'clean_database') {
+        await prisma.user.deleteMany({})
+    }
+
+    res.end()
+}
